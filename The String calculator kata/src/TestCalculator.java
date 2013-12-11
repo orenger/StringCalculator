234b00eb87854a43e45b3a6b9912d81dd57f0599
orenger <oren@expertnet.co.il>
2013-12-11 22:21:43 +0200

    start

---
+++ The String calculator kata/src/TestCalculator.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestCalculator {
+	
+	@Test
+	public void EmptyString_ReturnsZero() 
+	{
+		String str ="";
+		calculator calculator = new calculator();
+		int actual = calculator.calculate(str);
+		assertEquals(0, actual);
+	}
+
+}
---
+++ The String calculator kata/src/calculator.java
@@ -0,0 +1,11 @@
+
+public class calculator 
+{
+	public int calculate(String string)
+	{
+		if(string == "")
+			return -1;
+		return -2;
+	}
+	
+}
