43e6eb7c988fed778bbd5547cdaa831815d2ba20
orenger <oren@expertnet.co.il>
2013-12-11 22:52:42 +0200

    start

--- The String calculator kata/src/TestCalculator.java
+++ The String calculator kata/src/TestCalculator.java
@@ -8,7 +8,7 @@ public class TestCalculator {
 	@Test
 	public void EmptyString_ReturnsZero() 
 	{
-		String str ="";
+		String str ="1";
 		calculator calculator = new calculator();
 		int actual = calculator.calculate(str);
 		assertEquals(0, actual);
