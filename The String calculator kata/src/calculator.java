c7801addc1708ec8bde13fc829b25de72dac82f9
orenger <oren@expertnet.co.il>
2013-12-16 16:58:19 +0200

    after refactoring

--- The String calculator kata/src/calculator.java
+++ The String calculator kata/src/calculator.java
@@ -9,31 +9,15 @@ public class calculator
 		{
 			if(string.length()== 1)
 				return Integer.parseInt(string);
-			//String[] str = string.split(",",-1);
 			String delims = "[,.\n?!]+";
 			String [] str = string.split(delims,-1);
 			int sum = 0;
 			for(int i = 0 ; i<str.length ; i++ )
 				sum += calculate(str[i]);
 			return sum;
-			//return calculate(str[0])+calculate(str[1]);
-			//return calculate(Integer.parseInt(str[0]))+calculate(Integer.parseInt(str[1]));
 		}
 	}
 	
 	
-	String  splitter(String s)
-	{
-		String  splt = "";
-		
-		for(int i = 0; i<s.length() ; i++)
-		{
-			if (s.charAt(i) == '\n' )
-			{
-				splt+=',';
-			}
-			else splt+=s.charAt(i);
-		}
-		return splt;
-	}
+	
 }
